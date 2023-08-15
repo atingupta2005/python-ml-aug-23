e35d5a8bdb103aa394b06b084188e9350e317716
Atin Gupta <atingupta@email.com>
2023-08-15 16:59:52 +0530

    -

--- github.bat
+++ github.bat
@@ -1,4 +1,6 @@
 git checkout main
+del .gitignore
+copy .gitignore-github .gitignore
 git pull github-origin main
 git add *
 git commit -am "-"
--- gitlab.bat
+++ gitlab.bat
@@ -1,4 +1,6 @@
 git checkout am1
+del .gitignore
+copy .gitignore-gitlab .gitignore
 git pull gitlab-origin am1
 git add *
 git commit -am "-"
